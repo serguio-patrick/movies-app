8ad664f1bf1e80c87b1581a208a8c5146eaa9d39
SerguioL <serguio.l.landa@gmail.com>
2021-08-31 15:52:38 -0500

    addMovies works now and add a deleteMovies function

--- movies-app.js
+++ movies-app.js
@@ -6,28 +6,29 @@ const GLITCH = 'https://uneven-organic-meteoroid.glitch.me/movies';
 // http://www.omdbapi.com/?t=Universal+Soldier%3A+The+Return
 
 
-
+//gets movies
 function glitchFetch() {
     fetch(GLITCH)
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            data.forEach(function (moive){
-            $('#cards').append('<article>' +
-                '<p>' + moive.title + '</p>' +
-                '<p>' + moive.rating + '</p>' +
-                '<button className="edit">Edit</button>' +
-                '<button className="delete">Delete</button>' +
-                '</article>');
-        });
+            data.forEach(function (moive) {
+                $('#cards').append('<article>' +
+                    '<p>' + moive.title + '</p>' +
+                    '<p>' + moive.rating + '</p>' +
+                    '<button class="edit">Edit</button>' +
+                    '<button class="delete">Delete</button>' +
+                    '</article>');
+            });
         })
         .catch(console.error);
 }
 
+// call function
 glitchFetch();
 
 
-
+//adds Movies
 function addMovies(title, rating) {
     fetch(GLITCH, {
         method: 'POST',
@@ -39,40 +40,69 @@ function addMovies(title, rating) {
             rating: rating
         })
     }).then(response => response.json())
-        .then(data => {
-            // console.log(data)
-            data.forEach(function (movie){
-                $('#cards').append('<article>' +
-                    '<p>' + movie.title + '</p>' +
-                    '<p>' + movie.rating + '</p>' +
-                    '<button className="edit">Edit</button>' +
-                    '<button className="delete">Delete</button>' +
-                    '</article>');
-            });
+       .then(data => {
+            console.log(data)
+           $("article").remove();
+           glitchFetch();
+            // data.forEach(function (movie) {
+            //     $('#cards').append('<article>' +
+            //         '<p>' + movie.title + '</p>' +
+            //         '<p>' + movie.rating + '</p>' +
+            //         '<button class="edit">Edit</button>' +
+            //         '<button class="delete">Delete</button>' +
+            //         '</article>');
+            // });
         })
         .catch(console.error)
 }
 
+function deleteMovies() {
+    fetch(GLITCH, {
+        method: 'DELETE',
+        headers: {
+            'Content-TYpe': 'application/json'
+        }
+    }).then(response => response.json())
+        .then(data => {
+            console.log(data)
+            $("article").remove();
+            glitchFetch();
+            // data.forEach(function (movie) {
+            //     $('#cards').append('<article>' +
+            //         '<p>' + movie.title + '</p>' +
+            //         '<p>' + movie.rating + '</p>' +
+            //         '<button class="edit">Edit</button>' +
+            //         '<button class="delete">Delete</button>' +
+            //         '</article>');
+            // });
+        })
+        .catch(console.error)
+}
+
+//when clicked add a movie to json obj
 $('#addbutton').click(function () {
     addMovies($('#addtitle').val(), $('#addrating').val());
-    // glitchFetch();
+
+});
+
+$('.delete').click(function () {
+    deleteMovies($(this));
+
 });
 
 
 // const HOOKBIN = 'https://hookb.in/W1N8ZGelp8CYplzzpDPD';
 
-
-
-
+// loading screen
 document.onreadystatechange = function () {
     var state = document.readyState
     if (state === 'interactive') {
-        document.getElementById('contents').style.visibility="hidden";
+        document.getElementById('contents').style.visibility = "hidden";
     } else if (state === 'complete') {
-        setTimeout(function(){
+        setTimeout(function () {
             document.getElementById('interactive');
-            document.getElementById('load').style.visibility="hidden";
-            document.getElementById('contents').style.visibility="visible";
-        },1000);
+            document.getElementById('load').style.visibility = "hidden";
+            document.getElementById('contents').style.visibility = "visible";
+        }, 1000);
     }
 }
