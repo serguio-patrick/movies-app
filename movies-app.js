c76751b151f25175d92f77266bf5ffc2a8976d95
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-09-02 08:53:23 -0500

    styling

--- movies-app.js
+++ movies-app.js
@@ -224,12 +224,12 @@ $('#addButton').click(function (e) {
 document.onreadystatechange = function () {
     var state = document.readyState
     if (state === 'interactive') {
-        document.getElementById('contents').style.visibility = "hidden";
+        // document.getElementById('contents').style.visibility = "hidden";
     } else if (state === 'complete') {
         setTimeout(function () {
-            document.getElementById('interactive');
+            // document.getElementById('interactive');
             document.getElementById('load').style.visibility = "hidden";
-            document.getElementById('contents').style.visibility = "visible";
+            // document.getElementById('contents').style.visibility = "visible";
         }, 1000);
     }
 }
